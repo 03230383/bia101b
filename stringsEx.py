3ce378e65616cc24966b9b3017c3128596b3046e
03230383 <03230383.gcbs@rub.edu.bt>
2024-03-07 15:10:59 +0600

    string exercise

---
+++ stringsEx.py
@@ -0,0 +1,17 @@
+# get user input
+# convert it into ALL upper case
+# print the converted string
+
+userInput = input("Enter a string: ")
+userInputConverted = userInput.upper()
+print(userInputConverted)
+
+# A - 4
+# o - 0
+# e - 3
+# l - 1
+# s - 5
+# g - 9
+# b - 6
+# t - 7
+# z - 2
