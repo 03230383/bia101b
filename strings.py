fb583db82098951e2c90817154501ded6a578de0
03230383 <03230383.gcbs@rub.edu.bt>
2024-03-07 15:01:03 +0600

    string manipulation

---
+++ strings.py
@@ -0,0 +1,35 @@
+#string manipulation
+
+#string concatenation
+string1 = "hello "
+string2 = "world"
+
+combined = string1 + string2
+combined1 = "hi" + " " + "there"
+
+print('string concatenation')
+print(combined)
+print(combined1)
+
+#string interpolation
+print('string interpolation')
+#how do you print "john is 30 years of age"
+name = "john"
+age = 30
+interpolated = f"john is {age} years of age"
+print(interpolated)
+
+#string methods
+print('string methonds')
+#upper
+print('string upper')
+stringlower = 'HELLO HOW ARE YOU'
+converted = stringlower.upper()
+converted1 = "pure STRING".upper()
+print(converted)
+print(converted1)
+
+#capitalize
+print('string capitalize')
+capitalize = "hello there".capitalize()
+print(capitalize)
