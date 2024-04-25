30c5d2cfa09bcae9ee6deb2ef263665528a012ed
03230383 <03230383.gcbs@rub.edu.bt>
2024-04-25 15:03:51 +0600

    oop practice

---
+++ practice.py
@@ -0,0 +1,5 @@
+colors = ["red", "green", "blue"]
+is_present = "green" in colors
+print(is_present)  # Output: True
+test = "black" not in colors # Black doesn't exist? Return True if black doesn't exist. False if black exist
+print(test) 
---
+++ unit4/oop1.py
@@ -0,0 +1,6 @@
+class introduction:
+    def __init__(self, age, color):
+        self.age = age
+        self.color = color
+    def say_age(self):
+        words_to_say = " I am " + str(self.age) + " years old"
