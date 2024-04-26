614f96b236a39d62f7d1a7b4144e794f8f0cc122
03230383 <03230383.gcbs@rub.edu.bt>
2024-04-26 12:00:48 +0600

    calculation floor

--- unit4/oop1.py
+++ unit4/oop1.py
@@ -1,6 +1,6 @@
-class introduction:
-    def __init__(self, age, color):
+class Cat:
+    def __init__(self, name, breed, age, color):
+        self.name = name
+        self.breed = breed
         self.age = age
         self.color = color
-    def say_age(self):
-        words_to_say = " I am " + str(self.age) + " years old"
